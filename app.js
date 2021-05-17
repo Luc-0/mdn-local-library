bed72c21ae3f2f7d71be213dd6e7e1956a918c23
Luc-0 <luc0x00@gmail.com>
2021-05-17 13:30:09 -0300

    Add database with mongoose

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+require('dotenv/config');
+var mongoose = require('mongoose');
+var mongoDB = `mongodb+srv://luc:${process.env.ATLAS_PASSWORD}@cluster0.tylop.mongodb.net/local-library?retryWrites=true&w=majority`;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
